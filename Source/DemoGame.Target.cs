e51d9126edd66ff73b3366166e06080b074e949e
Alexandru Oprea <oprea_alexandrum@yahoo.com>
2025-12-04 15:53:03 +0200

    update build settings to latest

--- Source/DemoGame.Target.cs
+++ Source/DemoGame.Target.cs
@@ -8,8 +8,8 @@ public class DemoGameTarget : TargetRules
 	public DemoGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("DemoGame");
 	}
 }
